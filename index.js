2271ddc978b2e526385f2aef20fbcb1645d3b50d
khh-Niloy <niloy.dev.101@gmail.com>
2024-09-23 21:42:59 +0600

    added date()

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function setTextValue(id, value) {
 }
 
 
-// tabSwitch
+// tab Switch
 function tabSwitch(id) {
   document.getElementById("donationSection").classList.add("hidden");
   document.getElementById("historySection").classList.add("hidden");
@@ -15,6 +15,7 @@ function tabSwitch(id) {
   document.getElementById(id).classList.remove("hidden");
 }
 
+//tab color Switch
 document.getElementById("donationTab").addEventListener("click", (e) => {
   tabSwitch("donationSection");
 
@@ -40,17 +41,15 @@ function historyNewElement(inputValue, setTextId) {
     "rounded-xl",
     "my-5"
   );
-  newDiv.innerHTML = `<h1 class="font-semibold"> ${inputValue} Taka is ${
-    document.getElementById(setTextId).innerText
-  } </h1>
-        <p class="font-light text-sm text-black/80 mt-2"> Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)</p>`;
+  newDiv.innerHTML =
+  `<h1 class="font-semibold"> ${inputValue} Taka is ${document.getElementById(setTextId).innerText} </h1>
+  <p class="font-light text-sm text-black/80 mt-2"> Date : ${Date(Date.now()).toString()}</p>`;
   document.getElementById("historySection").appendChild(newDiv);
 }
 
-
+// extra validation
 function checkIsAllDigit(value){
   let check = false;
-
   for ( let i of value ) {
       if ( isNaN(i)==false ) {
           check = true;
@@ -71,88 +70,86 @@ let noakhaliTotalDonation = 0;
 document.getElementById("donateNowButton1").addEventListener("click", (e) => {
   let noakhaliInputValue = getInputFromId("noakhaliInput");
 
-
   if (isNaN(noakhaliInputValue) == true || noakhaliInputValue < 0 || checkIsAllDigit(document.getElementById('noakhaliInput').value)==false) {
     alert("Please enter valid amount");
     document.getElementById("noakhaliInput").value = "";
-  } else {
+  }
+  else {
     if (newMyBalance < noakhaliInputValue) {
       alert("insufficient balance");
       document.getElementById("noakhaliInput").value = "";
-    } else {
-      setTextValue(
-        "noakhaliTotal",
-        `${(noakhaliTotalDonation += noakhaliInputValue)} BDT`
-      );
-
+    }
+    else {
+      setTextValue("noakhaliTotal",`${(noakhaliTotalDonation += noakhaliInputValue)} BDT`);
       newMyBalance = newMyBalance - noakhaliInputValue;
       document.getElementById("myBalance").innerText = `${newMyBalance} BDT`;
       document.getElementById("noakhaliInput").value = "";
-
       historyNewElement(noakhaliInputValue, "noyakhaliHeadText");
       //modal
-      document
-        .getElementById("donateNowButton1")
-        .setAttribute("onclick", my_modal_1.showModal());
+      document.getElementById("donateNowButton1").setAttribute("onclick", my_modal_1.showModal());
     }
   }
 });
+
+
 
 let feniTotalDonation = 0;
 document.getElementById("donateNowButton2").addEventListener("click", (e) => {
   let feniInputValue = getInputFromId("feniInput");
-  if (isNaN(feniInputValue) == true || feniInputValue < 0) {
+
+  if (isNaN(feniInputValue) == true || feniInputValue < 0 || checkIsAllDigit(document.getElementById('feniInput').value)==false) {
     alert("Please enter valid amount");
     document.getElementById("feniInput").value = "";
-  } else {
+  }
+  else
+  {
     if (newMyBalance < feniInputValue) {
       alert("insufficient balance");
       document.getElementById("feniInput").value = "";
-    } else {
+    }
+    else {
       setTextValue("feniTotal", `${(feniTotalDonation += feniInputValue)} BDT`);
-
       newMyBalance = newMyBalance - feniInputValue;
       document.getElementById("myBalance").innerText = `${newMyBalance} BDT`;
       document.getElementById("feniInput").value = "";
-
       historyNewElement(feniInputValue, "feniHeadText");
       //modal
-      document
-        .getElementById("donateNowButton1")
-        .setAttribute("onclick", my_modal_1.showModal());
+      document.getElementById("donateNowButton1").setAttribute("onclick", my_modal_2.showModal());
     }
   }
 });
+
+
+
 
 let quotaTotalDonation = 0;
 document.getElementById("donateNowButton3").addEventListener("click", (e) => {
   let quotaInputValue = getInputFromId("quotaInput");
-  if (isNaN(quotaInputValue) == true || quotaInputValue < 0) {
+
+  if (isNaN(quotaInputValue) == true || quotaInputValue < 0 || checkIsAllDigit(document.getElementById('quotaInput').value)==false) {
     alert("Please enter valid amount");
     document.getElementById("quotaInput").value = "";
-  } else {
+  }
+  else
+  {
     if (newMyBalance < quotaInputValue) {
       alert("insufficient balance");
       document.getElementById("quotaInput").value = "";
-    } else {
-      setTextValue(
-        "quotaTotal",
-        `${(quotaTotalDonation += quotaInputValue)} BDT`
-      );
-
+    }
+    else {
+      setTextValue("quotaTotal", `${(quotaTotalDonation += quotaInputValue)} BDT`);
       newMyBalance = newMyBalance - quotaInputValue;
       document.getElementById("myBalance").innerText = `${newMyBalance} BDT`;
       document.getElementById("quotaInput").value = "";
-
       historyNewElement(quotaInputValue, "quotaHeadText");
       //modal
-      document
-        .getElementById("donateNowButton1")
-        .setAttribute("onclick", my_modal_1.showModal());
+      document.getElementById("donateNowButton1").setAttribute("onclick", my_modal_3.showModal());
     }
   }
 });
 
+
+// go to blog
 document.getElementById("blogButton").addEventListener("click", (e) => {
   window.location.assign("blog.html");
 });
